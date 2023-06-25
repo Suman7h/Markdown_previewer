68ae9e1556483855524a838822936180d84bd54c
Venkata Sumanth Peruri <sumanth@Venkatas-Air.lan>
2023-06-24 20:26:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import {marked} from 'marked'
+import "./App.css"
 
 function App() {
+  const [mark,setMark] = useState(` 
+  # Welcome to my React Markdown Previewer! 
+  ## Write anything here to see on the right side
+  [Example page](https://www.example.com)
+
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+  - First item
+  - Second item
+  - Third item
+  > blockquote
+
+  ![alt text](image.jpg)
+
+  **bold text**
+  `)
+
+  marked.setOptions({
+    breaks:true
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <textarea id="editor" value={mark} onChange={ e=>{setMark(e.target.value)}}></textarea>
+        <article id="preview" 
+        dangerouslySetInnerHTML={{
+          __html: marked(mark),
+         }}></article>
+      </div>
     </div>
   );
 }
